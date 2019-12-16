fbd09a480194c549f8b7c51a3b93adc3c7200dc5
Marius Gripsgard <marius@ubports.com>
2019-12-16 23:44:03 +0100

    Cleanup

--- src/utq.sh
+++ src/utq.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Copyright (C) 2019 UBports Foundation.
-# 
+#
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License version 3, as published by
 # the Free Software Foundation.
-# 
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
 # SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -151,9 +151,6 @@ case $1 in
     "ssh")
         ssh
         ;;
-    "debug")
-        debug $@
-	    ;;
     *)
         usage
         ;;
